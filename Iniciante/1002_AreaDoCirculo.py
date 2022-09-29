cc7588af22aec9e69a7a76a686fbececda9ad6ea
Giovani Ornellas <111402558+Giovanni-Ornellas@users.noreply.github.com>
2022-09-29 18:22:08 -0300

    Feito

---
+++ Iniciante/1002_AreaDoCirculo.py
@@ -0,0 +1,8 @@
+##Define um valor para a variável n
+n = 3.14159
+##Recebe o valor de raio e converte para float
+raio = float(input())
+##Calcula o valor da area do circulo
+area = n*pow(raio,2)
+##Imprime o valor obtido
+print(f'A={area:.4f}')
