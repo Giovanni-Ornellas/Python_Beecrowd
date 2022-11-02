47ee4fad9d196738db169716ba91250460df32ef
Giovani Ornellas <111402558+Giovanni-Ornellas@users.noreply.github.com>
2022-11-02 02:19:32 -0300

    feito

---
+++ Iniciante/1017_GastoDeCombustivel.py
@@ -0,0 +1,11 @@
+# Tempo gasto viagem
+tempo = int(input())
+
+# Velocidade Média
+velocidade = int(input())
+
+#Calcula a quantidade litros
+litros = (tempo*velocidade)/12
+
+#Imprime a velocidade
+print(f'{litros:.3f}')
